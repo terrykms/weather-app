b0f0acc73313b367b7e55ad0100dad03f1e70544
Kim Minseo <terrykms30@gmail.com>
2022-06-07 17:12:08 +0800

    initial commit

--- scripts.js
+++ scripts.js
@@ -3,6 +3,37 @@ const GEOCODING_API = "http://api.openweathermap.org/geo/1.0/direct";
 const API_KEY = "1501a965f1790381be648537630b699a";
 
 $(document).ready(function() {
+    $("#city").keyup(function() {
+        $(".suggestion").remove(); // clear suggestions
+        var keyword = $(this).val(); // current value in input
+
+        if (keyword != "") {
+            $.ajax({
+                url: GEOCODING_API + "?q=" + keyword + "&limit=5" + "&appid=" + API_KEY,
+                dataType: "json",
+                success: function(result) {
+                    result.forEach(item => {
+                        $("<div />", {
+                            text: item.name + ", " + item.country + (item.state ? (", " + item.state) : ""),
+                            class: "suggestion"
+                        }).appendTo("#searchResults");
+                    })
+                },
+                error: function(xhr, status, error) {
+                    console.log("Error: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+                }          
+            })
+        }
+    })
+
+    // relook at this code
+   $(".suggestion").on("click", function() {
+       var search = $(this).text();
+       console.log(search)
+       $("#city").val(search);
+   })
+
+
     $("#submitBtn").on("click", function() {
         var city = $("#city").val()
         getCityCoordinates(city);
